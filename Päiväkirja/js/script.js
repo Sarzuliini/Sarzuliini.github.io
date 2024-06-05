569e2af1b33e4e5ae28b346a433130413657d5fe
Sarita Tuominen <143175753+Sarzuliini@users.noreply.github.com>
2024-06-05 10:39:27 +0300

    UUSI TJK PÄIVÄKIRJA

--- Päiväkirja/js/script.js
+++ Päiväkirja/js/script.js
@@ -1,11 +1,18 @@
-const readMoreBtn = document.querySelector('.read-more-btn');
-const text = document.querySelector('.text');
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-readMoreBtn.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
-    if(readMoreBtn.innerText === 'Read More'){
-        readMoreBtn.innerText = 'Read Less';
-    }else{
-        readMoreBtn.innerText = 'Read More';
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-})
+  });
+}
